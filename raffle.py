7ea1ca130d9a47268567d6ac79ba5d7ec6834cfa
Jimmothyyy <james.philip.hart@gmail.com>
2022-02-24 09:52:50 +1300

    start ask loop

--- raffle.py
+++ raffle.py
@@ -1,1 +1,19 @@
-print()
+import random
+'''Declare list'''
+names = []
+'''Declare variables'''
+'''Run loop to append viable input names and remove empty answers'''
+print ("Please enter the names of people entering the raffle:\n")
+input_loop = True
+while input_loop:
+    try:
+        name = input()
+        if name == "stop":
+            input_loop = False
+        elif len(name) == 0:
+            print ("Input empty, please enter the names of people entering the raffle:\n")
+        else:
+            names.append(name)
+print (names)
+
+
