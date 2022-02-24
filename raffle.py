8e5fa49846bec763ec8b9f0c33cfe6e80ada5788
Jimmothyyy <james.philip.hart@gmail.com>
2022-02-25 09:25:58 +1300

    Update raffle.py

--- raffle.py
+++ raffle.py
@@ -1,9 +1,10 @@
+'''Importing '''
 import random
 '''Declare list'''
 names = []
-'''Declare variables'''
+print("Welcome to the best raffle you've ever seen, literally.\nPlease enter the name of each contestant once for each time they wish to put their name in")
 '''Run loop to append viable input names and remove empty answers'''
-print ("Please enter the names of people entering the raffle:\n")
+print ("Please enter the names of people entering the raffle:")
 input_loop = True
 while input_loop:
     try:
@@ -11,9 +12,11 @@ while input_loop:
         if name == "stop":
             input_loop = False
         elif len(name) == 0:
-            print ("Input empty, please enter the names of people entering the raffle:\n")
+            print ("Input empty, please enter the names of people entering the raffle:")
         else:
             names.append(name)
-print (names)
+    except:
+        print ("Please enter a valid name")
+
 
 
