aaff92402cf85624227b96e7bcc29ae445afd7f1
Jimmothyyy <james.philip.hart@gmail.com>
2022-02-28 12:03:28 +1300

    Raffle Final

--- raffle.py
+++ raffle.py
@@ -2,15 +2,30 @@
 import random
 '''Declare list'''
 names = []
-prize = input("Welcome to the best raffle you've ever seen, literally.\nPlease enter the prize to be won: ")
-prize_cost = input("How valuable is the prize?(in $'s, do not add the $ symbol): ")
+'''Loop to get valid prize input'''
+prize_input_loop = True
+while prize_input_loop:
+    prize = input("Welcome to the best raffle you've ever seen, literally.\nPlease enter the prize to be won: ")
+    if len(prize) == 0:
+        print("Please enter a prize to be won: ")
+    else:
+        prize_input_loop = False
+'''Loop to get valid prize cost input'''
+prize_cost_loop = True
+while prize_cost_loop:
+    try:
+        prize_cost = input("How valuable is the prize?(in $'s, do not add the $ symbol): ")
+        if len(prize_cost) == 0:
+            print("Please enter the value of the prize to be won: ")   
+    except:
+        print("Please enter a valid integer prize value (in $'s, do not add the $ symbol)")
 '''Run loop to append viable input names and remove empty answers'''
 print ("Please enter the names of people entering the raffle:")
-input_loop = True
+name_input_loop = True
 while input_loop:
     try:
         name = input()
-        if name == "stop":
+        if name == "end":
             input_loop = False
         elif len(name) == 0:
             print ("Input empty, please enter the names of people entering the raffle:")
@@ -22,5 +37,4 @@ while input_loop:
 winner = random.randint(0, len(names) - 1)
 '''Print winner'''
 print(f"All names have now been entered...\nThe winner of the {prize} worth ${prize_cost} is...{names[winner]}!")
-
 
