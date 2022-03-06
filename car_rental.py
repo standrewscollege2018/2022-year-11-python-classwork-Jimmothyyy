db0861f32f3b7e05940d8b3b1fb97a890230e1ad
Jimmothyyy <james.philip.hart@gmail.com>
2022-03-07 12:36:33 +1300

    Car rental Start
    Defined list and working on printing function for outputted text

---
+++ car_rental.py
@@ -0,0 +1,17 @@
+'''Define lists'''
+cars_rental_list = [["Suzuki Van (2)", True],
+                    ["Toyota Corolla (4)", True],
+                    ["Honda CRV (4)", True],
+                    ["Suzuki Swift (4)", True],
+                    ["Mitsubishi Airtrek (4)", True],
+                    ["Nissan DC Ute (4)", True],
+                    ["Toyota Previa (7)", True],
+                    ["Toyota Hi Ace (12)", True],
+                    ["Toyota Hi Ace (12)", True]]
+i = int(input())
+cars_rental_list[i-1].append("Unavailabe")
+for car in cars_rental_list:
+    for specifics in cars_rental_list[]:
+        print (specifics)
+        
+cars_rental_list[0][1]
