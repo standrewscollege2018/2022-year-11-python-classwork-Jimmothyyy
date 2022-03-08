454628630ef1d52b4b2ec1afb277e4cd5b391d36
Jimmothyyy <james.philip.hart@gmail.com>
2022-03-09 11:36:26 +1300

    working once car rental
    failing on second loop through

--- car_rental.py
+++ car_rental.py
@@ -1,3 +1,5 @@
+'''Set bypass variable'''
+bypass_loop = True
 '''Define lists'''
 cars_rental_list = [["Suzuki Van", "(2)", True],
                     ["Toyota Corolla", "(4)", True],
@@ -22,21 +24,38 @@ while run:
     '''Get rental input'''
     rental_input = True
     while rental_input:
-        i = int(input("Which car would you like to book? "))
-        if cars_rental_list[i-1][2] == False:
-            print(f"The {cars_rental_list[i-1][0]} is currently booked.")
-        elif i < 1 or i > 8:
-            print("Not a valid input, please enter an integer between 1 and 8.")
-        else:
-            rental_input = False
-    '''Assign unavailable status'''
-    cars_rental_list[i-1][2] = False
-    print(f"You have booked the {cars_rental_list[i-1][0]}.")
-    name = input("What is your name? ")
-    cars_rental_list[i-1].append(name)
-    print(f"Thanks, {name}.")
-    
-
+        try:
+            i = int(input("Which car would you like to book? (Press 0 to end rental day) "))          
+            if i < 0 or i > 8:
+                print("Not a valid input, please enter an integer between 1 and 8.")
+            elif i == 0:
+                rental_input = False
+                bypass_loop = False
+                run = False
+            elif cars_rental_list[i-1][2] == False:
+                print(f"The {cars_rental_list[i-1][0]} is currently booked.")     
+            elif cars_rental_list[i-1][2] == True:
+                rental_input = False
+        except ValueError:
+            print("That is not a valid integer between 0 - 8")
+    '''Assign availability status'''
+    while bypass_loop:
+        cars_rental_list[i-1][2] = False
+        print(f"You have booked the {cars_rental_list[i-1][0]}.")
+        name_loop = True
+        while name_loop:
+            name = input("What is your name? ")
+            if len(name.replace(" ", "")) == 0:
+                print("No name entered, please enter a name.")
+            else:
+                name_loop = False
+                bypass_loop = False
+                cars_rental_list[i-1].append(name)
+        print(f"Thanks, {name}.")
+print("Daily summary: ")
+for car in cars_rental_list:
+    if car[2] == False:
+        print (car[0], car[3])
             
         
             
