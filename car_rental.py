243db4613b7f83af2d51c4f0c414ff60f4c21c8e
Jimmothyyy <james.philip.hart@gmail.com>
2022-03-10 09:13:00 +1300

    Car rental finished?
    Not fully tested yet, but working as far as I can tell.

--- car_rental.py
+++ car_rental.py
@@ -1,5 +1,7 @@
-'''Set bypass variable'''
+'''Set variables'''
 bypass_loop = True
+end_code = False
+booked_cars = 0
 '''Define lists'''
 cars_rental_list = [["Suzuki Van", "(2)", True],
                     ["Toyota Corolla", "(4)", True],
@@ -13,6 +15,14 @@ cars_rental_list = [["Suzuki Van", "(2)", True],
 '''Begin run loop'''
 run = True
 while run:
+    '''Check if all car have been booked'''
+    if booked_cars == 9:
+        rental_input = False
+        bypass_loop = False
+        run = False                    
+        print("All cars have been booked for today.")    
+        break
+    bypass_loop = True
     print("Hello and welcome to Aperture Science's car rental program!")
     print("Here are a list of available cars: ")
     '''Fetch car name, seat number and availablility'''
@@ -26,22 +36,30 @@ while run:
     while rental_input:
         try:
             i = int(input("Which car would you like to book? (Press 0 to end rental day) "))          
-            if i < 0 or i > 8:
-                print("Not a valid input, please enter an integer between 1 and 8.")
+            if i < 0 or i > 9:
+                print("Not a valid input, please enter an integer between 1 and 9.")
             elif i == 0:
-                rental_input = False
-                bypass_loop = False
-                run = False
+                if booked_cars >= 1:
+                    rental_input = False
+                    bypass_loop = False
+                    run = False
+                elif booked_cars == 0:
+                    rental_input = False
+                    bypass_loop = False
+                    run = False                    
+                    print("No cars have been booked today. ")
+                    end_code = True
             elif cars_rental_list[i-1][2] == False:
                 print(f"The {cars_rental_list[i-1][0]} is currently booked.")     
             elif cars_rental_list[i-1][2] == True:
                 rental_input = False
         except ValueError:
-            print("That is not a valid integer between 0 - 8")
+            print("That is not a valid integer between 0 - 9")
     '''Assign availability status'''
     while bypass_loop:
         cars_rental_list[i-1][2] = False
         print(f"You have booked the {cars_rental_list[i-1][0]}.")
+        booked_cars += 1
         name_loop = True
         while name_loop:
             name = input("What is your name? ")
@@ -49,14 +67,16 @@ while run:
                 print("No name entered, please enter a name.")
             else:
                 name_loop = False
-                bypass_loop = False
-                cars_rental_list[i-1].append(name)
-        print(f"Thanks, {name}.")
-print("Daily summary: ")
-for car in cars_rental_list:
-    if car[2] == False:
-        print (car[0], car[3])
-            
+                cars_rental_list[i-1].append(name.title())
+        print(f"Thanks, {name.title()}.")
+        bypass_loop = False
+'''Daily summary'''
+if end_code == False:
+    print("Daily summary: ")
+    for car in cars_rental_list:
+        if car[2] == False:
+            print (car[0], car[3])
+
         
             
         
